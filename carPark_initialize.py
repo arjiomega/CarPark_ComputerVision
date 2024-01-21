6194bbf22ceb04a6aac29c23e11e0f1693aa6dd0
Richard Omega <richardjoseph.omega@gmail.com>
2024-01-21 08:40:32 +0800

    only kept for reference. to be removed.

--- carPark_initialize.py
+++ carPark_initialize.py
@@ -1,47 +1,51 @@
+# TODO: Remove. Only kept for reference.
+
 import cv2
 import pickle
 import numpy as np
 from pathlib import Path
 
-DATA_DIR = Path(Path().absolute(),'data')
+DATA_DIR = Path(Path().absolute(), "data")
 
-img = cv2.imread(str(Path(DATA_DIR,'parking_img.png')))
+img = cv2.imread(str(Path(DATA_DIR, "parking_img.png")))
 
 
-width, height = (60,30)
+width, height = (60, 30)
 
 try:
-    with open('CarParkPos',"rb") as f:
-            posList = pickle.load(f)
+    with open("CarParkPos", "rb") as f:
+        posList = pickle.load(f)
 except:
     posList = []
 
 
-def mouseclick(events,x,y,flags,params):
+def mouseclick(events, x, y, flags, params):
     if events == cv2.EVENT_LBUTTONDOWN:
-        posList.append((x,y))
+        posList.append((x, y))
     if events == cv2.EVENT_RBUTTONDOWN:
-        for i,pos in enumerate(posList):
-            x1,y1 = pos
-            if x1 < x < x1+width and y1 < y < y1+height:
+        for i, pos in enumerate(posList):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
                 posList.pop(i)
 
-    with open('CarParkPos',"wb") as f:
-        pickle.dump(posList,f)
+    with open("CarParkPos", "wb") as f:
+        pickle.dump(posList, f)
 
     return 0
 
+
 def checkParkingSpace():
     for pos in posList:
-        cv2.rectangle(img,pos,(pos[0]+width,pos[1]+height),(255,0,255),2)
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
+
 
 cv2.namedWindow("image", cv2.WINDOW_GUI_NORMAL)
 while True:
-    img = cv2.imread(str(Path(DATA_DIR,'parking_img.png')))
-    #cv2.rectangle(img,(100,100),(200,150),(255,0,255),2)
+    img = cv2.imread(str(Path(DATA_DIR, "parking_img.png")))
+    # cv2.rectangle(img,(100,100),(200,150),(255,0,255),2)
 
     checkParkingSpace()
 
-    cv2.imshow("image",img)
-    cv2.setMouseCallback("image",mouseclick)
+    cv2.imshow("image", img)
+    cv2.setMouseCallback("image", mouseclick)
     cv2.waitKey(1)
--- load_data.py
+++ load_data.py
@@ -1,24 +1,27 @@
+# TODO: Remove. Only kept for reference.
+
 from pytube import YouTube
 import os
 import cv2
 from pathlib import Path
-if not os.path.exists('data'):
-    os.makedirs('data')
+
+if not os.path.exists("data"):
+    os.makedirs("data")
 
 # download the video
-YouTube("https://www.youtube.com/watch?v=ddmw7eCwROU").streams.filter(res='1080p',file_extension='mp4').first().download('./data',filename='parking_vid.mp4')
-
+YouTube("https://www.youtube.com/watch?v=yojapmOkIfg").streams.filter(
+    res="1080p", file_extension="mp4"
+).first().download("./data", filename="parking_vid.mp4")
 
 
 # take a single screenshot from the video
-DATA_DIR = Path(Path().absolute(),'data')
-cap = cv2.VideoCapture(str(Path(DATA_DIR,'parking_vid.mp4')))
+DATA_DIR = Path(Path().absolute(), "data")
 
+cap = cv2.VideoCapture(str(Path(DATA_DIR, "parking_vid.mp4")))
+counter = 0
 while True:
-    ret,frame = cap.read()
-
-    if ret:
-
-        cv2.imwrite(str(Path(DATA_DIR,'parking_img.png')),frame)
-
-        break
+    counter += 1
+    ret, frame = cap.read()
+    print(ret)
+    if ret and counter == 50:
+        cv2.imwrite(str(Path(DATA_DIR, "parking_img.png")), frame)
